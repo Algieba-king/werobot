34b4663260d3b11b1448e80119b575e5882514d0
Algieba-dean <algieba.king@gmail.com>
2019-07-18 21:40:56 +0800

    Create 01_urllib.py
    urllib使用

---
+++ 1/script/01_urllib.py
@@ -0,0 +1,13 @@
+from urllib import  request
+
+# response = request.urlopen('www.taobao.com') # 不能打開 因為沒有加http
+url = 'http://www.baidu.com'
+# url = 'http://taobao.com' #淘寶做了反爬？
+response = request.urlopen(url=url)
+web_status = response.status
+web_read = response.read()
+web_read_line=response.readline()
+print(web_read_line+b'\n')
+print(web_read)
+# print(type(response)+'\n')
+# print(response.status)
